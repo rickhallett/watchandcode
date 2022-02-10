90687c2a579ee414a99a8e93e83ffbc3912c16f1
Rick Hallett <rick.hallett@brandwatch.com>
2022-02-10 12:54:12 +0000

    (fix): prettier config

--- practical-javascript/todolist.js
+++ practical-javascript/todolist.js
@@ -4,11 +4,11 @@
  * ======
  */
 
-var todos = ["item 1", "item 2", "item 3"];
+var todos = ['item 1', 'item 2', 'item 3'];
 
-todos.push("item 4");
+todos.push('item 4');
 
-todos[0] = "item 1 updated";
+todos[0] = 'item 1 updated';
 
 todos.splice(3, 1);
 
